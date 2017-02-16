77cc631b25c944597023dc52a990da1f824cb285
Rommel Rico <me@rommelrico.com>
2017-02-16 14:17:38 -0800

    Processing Authentication Credentials
    Logging a User in
    Error Handling
    Activity Spinner
    Conditional Rendering of JSX
    Clearing the Form Spinner
    Handling Authentication Events
    Logging a user out

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null };
+
 	componentWillMount() {
 		// Initialize Firebase
 		firebase.initializeApp({
@@ -14,13 +16,42 @@ class App extends Component {
 		    storageBucket: "auth-12368.appspot.com",
 		    messagingSenderId: "394815104638"
 		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+					<CardSection>
+						<Button onPress={() => firebase.auth().signOut()} >
+							Log Out
+						</Button>
+					</CardSection>
+				);
+			case false:
+				return <LoginForm />;
+			default:
+				return (
+					<CardSection>
+						<Spinner size='large' />
+					</CardSection>
+				);
+		}
 	}
 
 	render() {
 		return (
 			<View>
 			    <Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 		);
 	}
